17215abb118c8bd7598edb4f637a6820d6278ab3
Nastassia Miadzvedz <nastassiamiadzvedz@MacBook-Air-Nastassia.local>
2024-03-01 16:16:05 +0100

    Update Task_2.py

--- homework/Nastassia_Haladushka/Homework_5/Task_2.py
+++ homework/Nastassia_Haladushka/Homework_5/Task_2.py
@@ -9,6 +9,8 @@ text_one = 'результат операции: 42'
 text_two = 'результат операции: 514'
 text_three = 'результат работы программы: 9'
 
-print(int(text_one[text_one.index(':') + 2:]) + 10)
-print(int(text_two[text_two.index(':') + 2:]) + 10)
-print(int(text_three[text_three.index(':') + 2:]) + 10)
+number_one = (int(text_one[text_one.index(':') + 2:]) + 10)
+number_two = (int(text_two[text_two.index(':') + 2:]) + 10)
+number_three = (int(text_three[text_three.index(':') + 2:]) + 10)
+
+print(number_one + number_two + number_three)
